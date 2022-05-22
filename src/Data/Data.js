8c29bcaee2db121d135f26960ccf6331bd0b18df
Sushant-Dhungana <sushantdhungana29@gmail.com>
2022-05-22 17:08:06 +0545

    final commit

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -89,20 +89,20 @@ export const CardsData = [
 export const UpdatesData = [
   {
     img: img1,
-    name: "Andrew Thomas",
-    noti: "has ordered Apple smart watch 2500mh battery.",
-    time: "25 seconds ago",
+    name: "ABC",
+    noti: "has ordered an stand for mac.",
+    time: "19 seconds ago",
   },
   {
     img: img2,
-    name: "James Bond",
-    noti: "has received Samsung gadget for charging battery.",
-    time: "30 minutes ago",
+    name: "DEF",
+    noti: "has received a power battery from Amazon.",
+    time: "20 minutes ago",
   },
   {
     img: img3,
-    name: "Iron Man",
-    noti: "has ordered Apple smart watch, samsung Gear 2500mh battery.",
-    time: "2 hours ago",
+    name: "GHI",
+    noti: "has ordered a fridge, a tv from daraz.",
+    time: "5 hours ago",
   },
 ];
